9a7d79e01c65ee07117e4ffef5ac7aed95ad852c
Mark Spiezio <mark@spiezio.net>
2020-08-26 17:03:01 -0400

    GameObject Randomness Refactoring - WIP

--- src/map.rs
+++ src/map.rs
@@ -168,8 +168,20 @@ fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
 }
 
 fn place_objects(room: Rect, map: &Map, objects: &mut Vec<GameObject>) {
+    use rand::distributions::{IndependentSample, Weighted, WeightedChoice};
     // choose random number of monsters
     let num_monsters = rand::thread_rng().gen_range(0, MAX_ROOM_MONSTERS + 1);
+    let monster_chances = &mut [
+        Weighted {
+            weight: 80,
+            item: "orc"
+        },
+        Weighted {
+            weight: 20,
+            item: "troll"
+        }
+    ];
+    let monster_choice = WeightedChoice::new(monster_chances);
 
     for _ in 0..num_monsters {
         // choose random spot for this monster
@@ -178,34 +190,37 @@ fn place_objects(room: Rect, map: &Map, objects: &mut Vec<GameObject>) {
 
         // only place it if the tile is not blocked
         if !is_blocked(x, y, map, objects) {
-            let mut monster = if rand::random::<f32>() < 0.8 {
-                // 80% chance of getting an orc
-                let mut orc = GameObject::new(x, y, 'o', "orc", DESATURATED_GREEN, true);
-                orc.fighter = Some(Fighter {
-                    max_hp: 10,
-                    hp: 10,
-                    defense: 0,
-                    power: 3,
-                    xp: 35,
-                    on_death: DeathCallback::Monster
-                });
-                orc.ai = Some(Ai::Basic);
-                orc
-            } else {
-                // 20% chance of getting a troll
-                let mut troll = GameObject::new(x, y, 'T', "troll", DARKER_GREEN, true);
-                troll.fighter = Some(Fighter {
-                    max_hp: 16,
-                    hp: 16,
-                    defense: 1,
-                    power: 4,
-                    xp: 100,
-                    on_death: DeathCallback::Monster
-                });
-                troll.ai = Some(Ai::Basic);
-                troll
+            let mut monster = match monster_choice.ind_sample(&mut rand::thread_rng()) {
+                "orc" => {
+                    let mut orc = GameObject::new(x, y, 'o', "orc", DESATURATED_GREEN, true);
+                    orc.fighter = Some(Fighter {
+                        max_hp: 10,
+                        hp: 10,
+                        defense: 0,
+                        power: 3,
+                        xp: 35,
+                        on_death: DeathCallback::Monster
+                    });
+                    orc.ai = Some(Ai::Basic);
+                    orc
+                }  
+                "troll" => {
+                    // 20% chance of getting a troll
+                    let mut troll = GameObject::new(x, y, 'T', "troll", DARKER_GREEN, true);
+                    troll.fighter = Some(Fighter {
+                        max_hp: 16,
+                        hp: 16,
+                        defense: 1,
+                        power: 4,
+                        xp: 100,
+                        on_death: DeathCallback::Monster
+                    });
+                    troll.ai = Some(Ai::Basic);
+                    troll
+                }
+                _ => unreachable!()
             };
-            
+                
             monster.alive = true;
             objects.push(monster);
         }
@@ -213,7 +228,26 @@ fn place_objects(room: Rect, map: &Map, objects: &mut Vec<GameObject>) {
 
     // choose random number of items
     let num_items = rand::thread_rng().gen_range(0, MAX_ROOM_ITEMS + 1);
-    
+    let item_chances = &mut [
+        Weighted {
+            weight: 70,
+            item: Item::Heal
+        },
+        Weighted {
+            weight: 10,
+            item: Item::Lightning
+        },
+        Weighted {
+            weight: 10,
+            item: Item::Fireball
+        },
+        Weighted {
+            weight: 10,
+            item: Item::Confuse
+        },                        
+    ];
+    let item_choice = WeightedChoice::new(item_chances);
+
     for _ in 0..num_items {
         // choose random spot for this item
         let x = rand::thread_rng().gen_range(room.x1 + 1, room.x2);
